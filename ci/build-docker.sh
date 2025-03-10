77e7e858a9bc1fb98922e4567d7a6db5b90de496
Sofus Albertsen <sofus.albertsen@eficode.com>
2025-03-10 21:16:03 +0100

    Update Gradle configuration and Docker build script; add JDK 11 and Gradle 6 support in Devbox

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
-REPO="ghcr.io/$docker_username/"
+# if docker username is not set, set REPO to empty string
+docker_username=${DOCKER_USERNAME:-}
+[[ -z "${docker_username}" ]] && REPO="" || REPO="ghcr.io/$docker_username/"
 echo "${REPO}"
 docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" app/
